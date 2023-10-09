0dd7e9302fda778bc68432c7dc78241eb6983536
Carlos <carlos.gomez311096@gmail.com>
2023-10-09 14:22:24 -0400

    feat: cleanup

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,10 @@
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
 
-import { authOptions } from "@/lib/auth"
-import { db } from "@/lib/db"
-import { getCurrentUser } from "@/lib/session"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
+// import { authOptions } from "@/lib/auth"
+// import { db } from "@/lib/db"
+// import { getCurrentUser } from "@/lib/session"
+// import { EmptyPlaceholder } from "@/components/empty-placeholder"
 import { DashboardHeader } from "@/components/header"
-import { PostCreateButton } from "@/components/post-create-button"
-import { PostItem } from "@/components/post-item"
 import { DashboardShell } from "@/components/shell"
 
 export const metadata = {
@@ -14,34 +12,34 @@ export const metadata = {
 }
 
 export default async function DashboardPage() {
-  const user = await getCurrentUser()
+  // const user = await getCurrentUser()
 
-  if (!user) {
-    redirect(authOptions?.pages?.signIn || "/login")
-  }
+  // if (!user) {
+  //   redirect(authOptions?.pages?.signIn || "/login")
+  // }
 
-  const posts = await db.post.findMany({
-    where: {
-      authorId: user.id,
-    },
-    select: {
-      id: true,
-      title: true,
-      published: true,
-      createdAt: true,
-    },
-    orderBy: {
-      updatedAt: "desc",
-    },
-  })
+  // const posts = await db.post.findMany({
+  //   where: {
+  //     authorId: user.id,
+  //   },
+  //   select: {
+  //     id: true,
+  //     title: true,
+  //     published: true,
+  //     createdAt: true,
+  //   },
+  //   orderBy: {
+  //     updatedAt: "desc",
+  //   },
+  // })
 
   return (
     <DashboardShell>
       <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <PostCreateButton />
+        {/* <PostCreateButton /> */}
       </DashboardHeader>
       <div>
-        {posts?.length ? (
+        {/* {posts?.length ? (
           <div className="divide-y divide-border rounded-md border">
             {posts.map((post) => (
               <PostItem key={post.id} post={post} />
@@ -56,7 +54,7 @@ export default async function DashboardPage() {
             </EmptyPlaceholder.Description>
             <PostCreateButton variant="outline" />
           </EmptyPlaceholder>
-        )}
+        )} */}
       </div>
     </DashboardShell>
   )
